bec20a41b1cf2760f57a983d4246b1ee6ed966b5
Tom Shaw <tomshaw21@gmail.com>
2024-03-01 07:17:54 -0600

    Update avif/webp quality defaults.

--- resources/config/config.php
+++ resources/config/config.php
@@ -54,6 +54,6 @@ return [
     */
     'create_webp' => env('MEDIABLE_CREATE_WEBP', true),
     'create_avif' => env('MEDIABLE_CREATE_AVIF', true),
-    'webp_quality' => env('MEDIABLE_WEBP_QUALITY', 40),
-    'avif_quality' => env('MEDIABLE_AVIF_QUALITY', 40),
+    'webp_quality' => env('MEDIABLE_WEBP_QUALITY', 80),
+    'avif_quality' => env('MEDIABLE_AVIF_QUALITY', 80),
 ];
