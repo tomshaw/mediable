b8b4a72bb31590992d62dedbadb5230106998969
Tom Shaw <tomshaw21@gmail.com>
2024-03-02 01:02:16 -0600

    Fixed upload attachment button loading state.

--- resources/views/tailwind/includes/toolbar.blade.php
+++ resources/views/tailwind/includes/toolbar.blade.php
@@ -7,8 +7,8 @@
   <div class="flex items-center justify-end gap-2">
     <button type="button" class="relative flex items-center justify-center px-4 py-1.5 gap-x-2 bg-[#555] text-white rounded-full text-xs font-normal cursor-pointer transition-all duration-100 ease-in" wire:click="clearFiles()">Reset</button>
     <button type="button" class="relative flex items-center justify-center px-4 py-1.5 gap-x-2 bg-[#555] text-white rounded-full text-xs font-normal cursor-pointer transition-all duration-100 ease-in" wire:click="createAttachments()" wire:loading.attr="disabled">
-      <span wire:loading.remove>Add Attachments</span>
-      <span wire:loading>Processing...</span>
+      <span wire:loading.remove wire:target="createAttachments">Add Attachments</span>
+      <span wire:loading wire:target="createAttachments">Processing...</span>
     </button>
   </div>
   @endif
