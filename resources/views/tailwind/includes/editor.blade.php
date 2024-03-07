0f60f593a4b9c41f48423d55b2256a747c12995e
Tom Shaw <tomshaw21@gmail.com>
2024-03-07 14:05:43 -0600

    Proportionalize editor container content.

--- resources/views/tailwind/includes/editor.blade.php
+++ resources/views/tailwind/includes/editor.blade.php
@@ -1,7 +1,7 @@
 <div class="flex items-center justify-center overflow-hidden p-4 md:p-6 lg:p-8 m-0 h-full bg-pattern">
   @if ($this->mimeTypeImage($this->model->fileType))
-  <div class="flex items-center justify-center" style="width: 40%;">
-    <img src="{{ asset($this->model->fileUrl) }}?id={{ $uniqueId }}" class="object-contain shadow-md max-w-full" data-id={{$this->model->id}}>
+  <div class="flex items-center justify-center">
+    <img src="{{ asset($this->model->fileUrl) }}?id={{ $uniqueId }}" class="object-contain shadow-md" style="max-height: 600px;">
   </div>
   @elseif ($this->mimeTypeVideo($this->model->fileType))
   <div class="flex items-center justify-center h-full w-full">
