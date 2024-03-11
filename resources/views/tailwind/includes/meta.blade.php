a6edcd09ca7b27a72525954d4cbdefb7d900b083
Tom Shaw <tomshaw21@gmail.com>
2024-03-11 15:13:11 -0500

    wip: 🚧 UI/UX improvements.

--- resources/views/tailwind/includes/meta.blade.php
+++ resources/views/tailwind/includes/meta.blade.php
@@ -70,22 +70,6 @@
                 </div>
                 @endif
 
-                @if ($show->isShowEditor() && count($selected))
-                <button wire:click="enableEditorMode()" class="group relative inline-flex h-7 items-center justify-center overflow-hidden w-full bg-[#555] px-4 text-xs font-normal text-white"><span>Editor</span>
-                    <div class="w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-0 group-hover:pl-1 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5">
-                            <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
-                        </svg></div>
-                </button>
-                @endif
-
-                @if ($show->isShowPreview() && count($selected))
-                <button wire:click="enablePreviewMode()" class="group relative inline-flex h-7 items-center justify-center overflow-hidden w-full bg-[#555] px-4 text-xs font-normal text-white"><span>Preview</span>
-                    <div class="w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-0 group-hover:pl-1 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5">
-                            <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
-                        </svg></div>
-                </button>
-                @endif
-
             </div>
         </div>
     </div>
--- resources/views/tailwind/includes/toolbar.blade.php
+++ resources/views/tailwind/includes/toolbar.blade.php
@@ -1,13 +1,13 @@
 <div class="flex items-center justify-between h-full w-full px-4">
 
   @if($panel->isUploadMode())
-  <div class="flex items-center justify-start gap-2">
+  <div class="flex items-center justify-start gap-1.5 xl:gap-2">
     <button wire:click="enableThumbMode" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white transition active:scale-95">
       <x-icons.close />
     </button>
   </div>
   @if(count($files) >= 1)
-  <div class="flex items-center justify-end gap-2">
+  <div class="flex items-center justify-end gap-1.5 xl:gap-2">
     <button wire:click="clearFiles" class="group relative inline-flex items-center justify-center overflow-hidden rounded-full bg-[#555] py-1.5 px-4 text-xs font-normal text-white">
       <span class="absolute h-0 w-0 rounded-full bg-red-500 transition-all duration-300 group-hover:h-56 group-hover:w-32"></span>
       <span class="relative">Reset</span>
@@ -22,7 +22,7 @@
   @endif
 
   @if($panel->isThumbMode())
-  <div class="flex items-center justify-start gap-2">
+  <div class="flex items-center justify-start gap-1.5 xl:gap-2">
 
     <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white" wire:click="toggleMetaInfo">
       <x-icons.arrow :show="$show->isShowMetaInfo()" direction="right" />
@@ -69,7 +69,15 @@
     @endif
 
   </div>
-  <div class="flex items-center justify-end gap-2">
+  <div class="flex items-center justify-end gap-1.5 xl:gap-2">
+
+    @if ($show->isShowEditor() && count($selected))
+    <button wire:click="enableEditorMode()" class="group relative inline-flex h-7 items-center justify-center rounded-full overflow-hidden bg-[#555] px-4 text-xs font-normal text-white"><span>Editor</span>
+      <div class="w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-0 group-hover:pl-1 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5">
+          <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+        </svg></div>
+    </button>
+    @endif
 
     @if ($show->isShowUpload())
     <button wire:click="enableUploadMode" class="group relative inline-flex h-7 items-center justify-center rounded-full bg-[#555] py-1.5 pl-5 pr-7 font-normal text-xs text-neutral-50"><span class="z-10 pr-2">Uploads</span>
@@ -88,7 +96,7 @@
   @endif
 
   @if($panel->isPreviewMode())
-  <div class="flex items-center justify-start gap-2">
+  <div class="flex items-center justify-start gap-1.5 xl:gap-2">
 
     <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white" wire:click="toggleMetaInfo">
       <x-icons.arrow :show="$show->isShowMetaInfo()" direction="right" />
@@ -103,7 +111,7 @@
     </div>
 
   </div>
-  <div class="flex flex-row items-center justify-end gap-2">
+  <div class="flex flex-row items-center justify-end gap-1.5 xl:gap-2">
 
     <button wire:click="deleteAttachment({{$attachment->id}})" class="group relative inline-flex items-center justify-center overflow-hidden rounded-full bg-[#555] py-1.5 px-4 text-xs font-normal text-white">
       <span class="absolute h-0 w-0 rounded-full bg-red-500 transition-all duration-300 group-hover:h-56 group-hover:w-32"></span>
@@ -125,7 +133,7 @@
   @endif
 
   @if($panel->isEditorMode())
-  <div class="flex items-center justify-start gap-2">
+  <div class="flex items-center justify-start gap-1.5 xl:gap-2">
     <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white" wire:click="toggleMetaInfo">
       <x-icons.arrow :show="$show->isShowMetaInfo()" direction="right" />
     </button>
@@ -138,7 +146,7 @@
       <div class="border-gray-300 h-6 w-6 animate-spin rounded-full border-2 border-t-blue-600"></div>
     </div>
   </div>
-  <div class="flex flex-row items-center justify-end gap-2">
+  <div class="flex flex-row items-center justify-end gap-1.5 xl:gap-2">
     <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white" wire:click="toggleSidebar">
       <x-icons.arrow :show="$show->isShowSidebar()" direction="left" />
     </button>
