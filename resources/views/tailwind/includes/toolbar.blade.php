bb85438d6a15ac53a2cd808d273f257c947ca789
Tom Shaw <tomshaw21@gmail.com>
2024-03-13 12:09:27 -0500

    wip: 🚧 UI/UX improvements.

--- resources/views/tailwind/includes/toolbar.blade.php
+++ resources/views/tailwind/includes/toolbar.blade.php
@@ -3,18 +3,18 @@
   @if($panel->isUploadMode())
   <div class="flex items-center justify-start gap-1.5 xl:gap-2">
     @if(!$data->isEmpty())
-    <button wire:click="enableThumbMode" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white transition active:scale-95">
+    <button wire:click="enableThumbMode" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-xs tracking-wider text-neutral-50 transition active:scale-95">
       <x-icons.close />
     </button>
     @endif
   </div>
   @if(count($files) >= 1)
   <div class="flex items-center justify-end gap-1.5 xl:gap-2">
-    <button wire:click="clearFiles" class="group relative inline-flex items-center justify-center overflow-hidden rounded-full bg-[#555] py-1.5 px-4 text-xs font-normal text-white">
+    <button wire:click="clearFiles" class="group relative inline-flex items-center justify-center overflow-hidden rounded-full bg-[#555] py-1.5 px-4 font-medium text-xs tracking-wider text-neutral-50">
       <span class="absolute h-0 w-0 rounded-full bg-red-500 transition-all duration-300 group-hover:h-56 group-hover:w-32"></span>
       <span class="relative">Reset</span>
     </button>
-    <button type="button" class="group relative inline-flex items-center justify-center overflow-hidden rounded-full bg-[#555] py-1.5 px-4 font-normal text-xs text-neutral-50" wire:click="createAttachments" wire:loading.attr="disabled">
+    <button type="button" class="group relative inline-flex items-center justify-center overflow-hidden rounded-full bg-[#555] py-1.5 px-4 font-medium text-xs tracking-wider text-neutral-50" wire:click="createAttachments" wire:loading.attr="disabled">
       <span class="absolute h-0 w-0 rounded-full bg-blue-400 transition-all duration-300 group-hover:h-full group-hover:w-full"></span>
       <span class="relative" wire:loading.remove wire:target="createAttachments">Submit</span>
       <span class="relative" wire:loading wire:target="createAttachments">Processing...</span>
@@ -26,7 +26,7 @@
   @if($panel->isThumbMode())
   <div class="flex items-center justify-start gap-1.5 xl:gap-2">
 
-    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white" wire:click="toggleMetaInfo">
+    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-xs tracking-wider text-neutral-50" wire:click="toggleMetaInfo">
       <x-icons.arrow :show="$show->isShowMetaInfo()" direction="right" />
     </button>
 
@@ -39,7 +39,7 @@
     @endif
 
     @if($show->isShowOrderDir() && $data->count())
-    <button type="button" class="relative flex items-center justify-between w-[80px] min-w-[80px] max-w-[80px] px-3 py-1.5 bg-[#555] text-white rounded-full text-xs font-normal cursor-pointer transition-all duration-100 ease-in" wire:click="toggleOrderDir()">
+    <button type="button" class="relative flex items-center justify-between w-[80px] min-w-[80px] max-w-[80px] px-3 py-1.5 bg-[#555] rounded-full font-medium text-xs tracking-wider text-neutral-50 cursor-pointer transition-all duration-100 ease-in" wire:click="toggleOrderDir()">
       @if(strtoupper($orderDir) == 'ASC')
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path>
@@ -91,7 +91,7 @@
     </button>
     @endif
 
-    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white" wire:click="toggleSidebar">
+    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-xs tracking-wider text-neutral-50" wire:click="toggleSidebar">
       <x-icons.arrow :show="$show->isShowSidebar()" direction="left" />
     </button>
   </div>
@@ -100,11 +100,11 @@
   @if($panel->isPreviewMode())
   <div class="flex items-center justify-start gap-1.5 xl:gap-2">
 
-    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white" wire:click="toggleMetaInfo">
+    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-xs tracking-wider text-neutral-50" wire:click="toggleMetaInfo">
       <x-icons.arrow :show="$show->isShowMetaInfo()" direction="right" />
     </button>
 
-    <button wire:click="enableThumbMode" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white transition active:scale-95">
+    <button wire:click="enableThumbMode" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-xs tracking-wider text-neutral-50 transition active:scale-95">
       <x-icons.close />
     </button>
 
@@ -115,20 +115,20 @@
   </div>
   <div class="flex flex-row items-center justify-end gap-1.5 xl:gap-2">
 
-    <button wire:click="deleteAttachment({{$attachment->id}})" class="group relative inline-flex items-center justify-center overflow-hidden rounded-full bg-[#555] py-1.5 px-4 text-xs font-normal text-white">
+    <button wire:click="deleteAttachment({{$attachment->id}})" class="group relative inline-flex items-center justify-center overflow-hidden rounded-full bg-[#555] py-1.5 px-4 font-medium text-xs tracking-wider text-neutral-50">
       <span class="absolute h-0 w-0 rounded-full bg-red-500 transition-all duration-300 group-hover:h-56 group-hover:w-32"></span>
       <span class="relative">Trash</span>
     </button>
 
     @if ($show->isShowEditor() && count($selected))
-    <button wire:click="enableEditorMode()" class="group relative inline-flex h-7 items-center justify-center overflow-hidden rounded-full bg-[#555] px-4 text-xs font-normal text-white"><span>Editor</span>
+    <button wire:click="enableEditorMode()" class="group relative inline-flex h-7 items-center justify-center overflow-hidden rounded-full bg-[#555] px-4 font-medium text-xs tracking-wider text-neutral-50"><span>Editor</span>
       <div class="w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-0 group-hover:pl-1 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5">
           <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
         </svg></div>
     </button>
     @endif
 
-    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white" wire:click="toggleSidebar">
+    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-xs tracking-wider text-neutral-50" wire:click="toggleSidebar">
       <x-icons.arrow :show="$show->isShowSidebar()" direction="left" />
     </button>
   </div>
@@ -136,11 +136,11 @@
 
   @if($panel->isEditorMode())
   <div class="flex items-center justify-start gap-1.5 xl:gap-2">
-    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white" wire:click="toggleMetaInfo">
+    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-xs tracking-wider text-neutral-50" wire:click="toggleMetaInfo">
       <x-icons.arrow :show="$show->isShowMetaInfo()" direction="right" />
     </button>
 
-    <button wire:click="closeImageEditor" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white transition active:scale-95">
+    <button wire:click="closeImageEditor" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-xs tracking-wider text-neutral-50 transition active:scale-95">
       <x-icons.close />
     </button>
 
@@ -149,7 +149,7 @@
     </div>
   </div>
   <div class="flex flex-row items-center justify-end gap-1.5 xl:gap-2">
-    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-white" wire:click="toggleSidebar">
+    <button type="button" class="group relative inline-flex h-7 w-7 items-center justify-center overflow-hidden rounded-full bg-[#555] font-medium text-xs tracking-wider text-neutral-50" wire:click="toggleSidebar">
       <x-icons.arrow :show="$show->isShowSidebar()" direction="left" />
     </button>
   </div>
